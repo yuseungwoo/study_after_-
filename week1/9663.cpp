4872266e9bf09b2a5c3ec9af5633764a511185e4
yuseungwoo <42972413+yuseungwoo@users.noreply.github.com>
2020-10-07 09:27:22 +0900

    Create 9663.cpp

---
+++ week1/9663.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int n, ans = 0;
+bool a[14], b[27], c[27];
+
+void dfs(int i) {
+	if (i == n) {
+		ans++;
+		return;
+	}
+	for (int j = 0; j < n; j++) {
+		if (!a[j] && !b[i + j] && !c[i - j + n - 1]) {
+			a[j] = b[i + j] = c[i - j + n - 1] = true;
+			dfs(i + 1);
+			a[j] = b[i + j] = c[i - j + n - 1] = false;
+		}
+	}
+}
+
+int main() {
+	cin >> n;
+	dfs(0);
+	cout << ans <<endl;
+	return 0;
+}
